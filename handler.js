f640fae41e12bbf82d9e98109890fa0f9b29e7b1
Bruno Marques <bruno@bmarques.net>
2018-10-09 00:32:03 -0300

    Fetching stock tickers

--- handler.js
+++ handler.js
@@ -1,10 +1,38 @@
-export const hello = context => {
-  context.log("JavaScript HTTP trigger function processed a request.");
+import cheerio from "cheerio";
+import { parseScript } from "esprima";
+import request from "request-promise";
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: "Go Serverless v1.x! Your function executed successfully!"
-  };
+const responseError = (context, err, message) => {
+  const body = { message: message, stacktrace: err };
+  context.log.error(message);
+  context.log.error(err);
+  context.res = { status: 500, body: JSON.stringify(body) };
+};
 
-  context.done();
+const tickerListOptions = {
+  uri: "http://www.fundamentus.com.br/script/cmplte.php",
+  transform: body => {
+    // this is VERY brittle and assumes the output starts out like this:
+    //    window.addEvent('domready', function(){
+    // 		  var searchInput = $('completar');
+    // 			var tokens = [
+    //      ...
+    const script = parseScript(body);
+    const elements =
+      script.body[0].expression.arguments[1].body.body[1].declarations[0].init
+        .elements;
+    return elements.map(expression => expression.elements[0].value);
+  }
+};
+
+export const getStocks = async context => {
+  context.log("Fetching list of stock tickers...");
+
+  let tickers;
+  try {
+    tickers = await request(tickerListOptions);
+  } catch (err) {
+    responseError(context, err, "Failure on obtaining stock tickers!");
+    return;
+  }
 };
