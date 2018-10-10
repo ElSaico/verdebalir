0b663c207a3f1fc0f90acd127090a9dade0fce17
Bruno Marques <bruno@bmarques.net>
2018-10-10 00:20:37 -0300

    Apifying

--- handler.js
+++ handler.js
@@ -1,79 +1,70 @@
-import Bottleneck from "bottleneck";
-import cheerio from "cheerio";
-import request from "request-promise";
+import Apify from "apify";
 import zipObject from "lodash.zipobject";
 
 const stockUrl = "http://www.fundamentus.com.br/detalhes.php";
 
-const throttle = new Bottleneck({ minTime: 500 });
-
-const responseError = (context, err, message) => {
-  const body = { message: message, stacktrace: err };
-  context.log.error(message);
-  context.log.error(err);
-  context.res = { status: 500, body: JSON.stringify(body) };
-};
-
-const tickerListOptions = {
-  uri: stockUrl,
-  transform: body => {
-    const $ = cheerio.load(body);
-    return $(".resultado td a")
-      .map((_, a) => $(a).text())
-      .get();
-  }
-};
-
-const parseIndicators = body => {
-  const $ = cheerio.load(body);
-  const table = $(".conteudo table:nth-of-type(3)");
-  const labels = table
-    .find("td.label:nth-child(n+2) .txt")
-    .map((_, cell) => $(cell).text())
-    .get();
-  const values = table
-    .find("td.data:nth-child(n+3) .txt")
-    .map((_, cell) =>
-      $(cell)
-        .text()
-        .trim()
-    )
-    .get();
-  return zipObject(labels, values);
-};
-
 export const getStocks = async context => {
-  context.log("Fetching list of stock tickers...");
+  const responseError = (err, message) => {
+    const body = { message: message, stacktrace: err };
+    context.log.error(message);
+    context.log.error(err);
+    context.res = { status: 500, body: JSON.stringify(body) };
+  };
 
-  let tickers;
-  try {
-    tickers = await request(tickerListOptions);
-  } catch (err) {
-    responseError(context, err, "Failure on obtaining stock tickers!");
-    return;
-  }
+  Apify.main(async () => {
+    context.log("Fetching list of stock tickers...");
 
-  tickers = await new Map(
-    tickers.map(async ticker => {
-      context.log(`Fetching indicators for stock ${ticker}...`);
-      let indicators;
-      try {
-        indicators = await throttle.schedule(() =>
-          request({
-            uri: stockUrl,
-            qs: { papel: ticker },
-            transform: parseIndicators
-          })
-        );
-      } catch (err) {
+    const tickerRequestQueue = await Apify.openRequestQueue();
+    const tickerCrawler = new Apify.CheerioCrawler({
+      tickerRequestQueue,
+      maxRequestsPerCrawl: 100,
+      maxConcurrency: 10,
+      handlePageFunction: async ({ request, html, $ }) => {
+        context.log(`Fetching indicators for stock ${ticker}...`);
+        const table = $(".conteudo table:nth-of-type(3)");
+        const labels = table
+          .find("td.label:nth-child(n+2) .txt")
+          .map((_, cell) => $(cell).text())
+          .get();
+        const values = table
+          .find("td.data:nth-child(n+3) .txt")
+          .map((_, cell) =>
+            $(cell)
+              .text()
+              .trim()
+          )
+          .get();
+        const indicators = zipObject(labels, values);
+        indicators.ticker = ticker;
+        await Apify.pushData(indicators);
+      },
+      handleFailedRequestFunction: async ({ request }) => {
         context.log.error(
           `Failure on obtaining indicators for stock ${ticker}`
         );
-        return [ticker, {}];
       }
-      return [ticker, indicators];
-    })
-  );
+    });
 
-  context.res = { body: JSON.stringify(tickers) };
+    const listRequestList = new Apify.RequestList({
+      sources: [{ url: stockUrl }]
+    });
+    await listRequestList.initialize();
+    const listCrawler = new Apify.CheerioCrawler({
+      listRequestList,
+      handlePageFunction: async ({ request, html, $ }) => {
+        $(".resultado td a").map(async (_, a) => {
+          const ticker = $(a).text();
+          await tickerRequestQueue.addRequest(
+            new Apify.Request({ url: stockUrl, qs: { papel: ticker } })
+          );
+        });
+        await tickerCrawler.run();
+      },
+      handleFailedRequestFunction: async ({ request }) => {
+        responseError(request, "Failure on obtaining stock tickers!");
+      }
+    });
+
+    await listCrawler.run();
+  });
 };
